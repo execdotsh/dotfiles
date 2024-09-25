86d988a50fb6549f5cf4c3d824f69ebde261eed0
Andrew Cirneck <execdotsh@gmail.com>
2024-09-25 10:55:34 +0500

    init.lua: use osc52 for clipboard

--- init.lua
+++ init.lua
@@ -19,7 +19,6 @@ end
 vim.opt.autoindent = true
 vim.opt.backspace = "2"
 vim.opt.belloff = "all"
-vim.opt.clipboard = ""
 vim.opt.completeopt = { "longest", "menuone" }
 vim.opt.copyindent = true
 vim.opt.encoding = "utf-8"
@@ -327,6 +326,36 @@ require("packer").startup(function(use)
 				require("gitsigns").nav_hunk("prev")
 			end)
 		end,
+	})
+
+	use({
+		"chrisgrieser/nvim-early-retirement",
+		["config"] = function ()
+			require("early-retirement").setup({})
+		end,
+	})
+
+	use({
+		"ojroques/nvim-osc52",
+		["config"] = function()
+			require('osc52').setup({
+				["max_length"] = 0,
+				["silent"] = false,
+				["trim"] = false,
+				["tmux_passthrough"] = true,
+			})
+			local function copy(lines, _)
+				require('osc52').copy(table.concat(lines, '\n'))
+			end
+			local function paste()
+				return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
+			end
+			vim.g.clipboard = {
+				["name"] =  "osc52",
+				["copy"] =  { ['+'] = copy, ['*'] = copy },
+				["paste"] = { ['+'] = paste, ['*'] = paste },
+			}
+		end
 	})
 
 end)
