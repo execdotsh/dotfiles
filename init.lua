8c05f3e6d7e0585c44aeeb8bbb12fce0c83182a4
Andrew Cirneck <execdotsh@gmail.com>
2025-06-20 20:14:31 +0500

    WIP

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 
 -- utility
 
+--require('github-theme').setup()
+
 function bufdir()
 	return vim.fn.expand("%:p:h")
 end
@@ -106,286 +108,199 @@ end)
 
 -- plugins
 
-require("packer").startup(function(use)
-
-	use("wbthomason/packer.nvim")
-
-	use({
-		"nvim-telescope/telescope.nvim",
-		["tag"] = "0.1.8",
-		["config"] = function()
-			require("telescope").setup({
-				["defaults"] = {
-					["borderchars"] = { "─", "│", "─", "│", "┌", "┐", "┘", "└" }
-				},
-			})
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', function()
-				if not pcall(builtin.git_files, { ["cwd"] = git_dir() }) then
-					builtin.find_files()
-				end
-			end)
-			vim.keymap.set('n', '<leader>fF', builtin.find_files)
-			vim.keymap.set('n', '<leader>fg', function()
-				builtin.live_grep({ ["cwd"] = git_dir() })
-			end)
-			vim.keymap.set('n', '<leader>fG', builtin.live_grep)
-			vim.keymap.set('n', '<leader>fb', builtin.buffers)
-			vim.keymap.set('n', '<leader>fj', builtin.jumplist)
-		end,
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
 	})
+end
+vim.opt.rtp:prepend(lazypath)
 
-	use({ "nvim-tree/nvim-web-devicons" })
+require("lazy").setup({
+	{ "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" }, config = function()
+		require("telescope").setup({ defaults = { borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" } } })
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", function()
+			if not pcall(builtin.git_files, { cwd = git_dir() }) then builtin.find_files() end
+		end)
+		vim.keymap.set("n", "<leader>fF", builtin.find_files)
+		vim.keymap.set("n", "<leader>fg", function() builtin.live_grep({ cwd = git_dir() }) end)
+		vim.keymap.set("n", "<leader>fG", builtin.live_grep)
+		vim.keymap.set("n", "<leader>fb", builtin.buffers)
+		vim.keymap.set("n", "<leader>fj", builtin.jumplist)
+	end },
 
-	use({
-		"jiaoshijie/undotree",
-		["requires"] = {
-			"nvim-lua/plenary.nvim",
-		},
-		["config"] = function()
-			local undotree = require('undotree')
-			undotree.setup({
-				["float_diff"] = true,
-				["layout"] = "left_bottom",
-				["position"] = "left",
-				["ignore_filetype"] = {
-					'undotree', 'undotreeDiff', 'qf', 'TelescopePrompt',
-					'spectre_panel', 'tsplayground'
-				},
-				["window"] = {
-					["winblend"] = 30,
-				},
-				["keymaps"] = {
-					['j'] = "move_next",
-					['k'] = "move_prev",
-					['gj'] = "move2parent",
-					['J'] = "move_change_next",
-					['K'] = "move_change_prev",
-					['<cr>'] = "action_enter",
-					['p'] = "enter_diffbuf",
-					['q'] = "quit",
-				},
-			})
-			vim.keymap.set('n', '<leader>u', require('undotree').toggle, { noremap = true, silent = true })
-		end,
-	})
+	{ "nvim-tree/nvim-web-devicons" },
 
-	if has_exec("node") then
-		use({
-			"neoclide/coc.nvim",
-			["branch"] = "release",
+	{ "jiaoshijie/undotree", dependencies = { "nvim-lua/plenary.nvim" }, config = function()
+		require("undotree").setup({
+			float_diff = true,
+			layout = "left_bottom",
+			position = "left",
+			ignore_filetype = { "undotree", "undotreeDiff", "qf", "TelescopePrompt", "spectre_panel", "tsplayground" },
+			window = { winblend = 30 },
+			keymaps = {
+				j = "move_next", k = "move_prev", gj = "move2parent", J = "move_change_next", K = "move_change_prev",
+				["<cr>"] = "action_enter", p = "enter_diffbuf", q = "quit"
+			},
 		})
-	end
+		vim.keymap.set("n", "<leader>u", require("undotree").toggle, { noremap = true, silent = true })
+	end },
 
-	use({
-		"nvim-neo-tree/neo-tree.nvim",
-		["requires"] = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		["config"] = function()
+	has_exec("node") and { "neoclide/coc.nvim", branch = "release" } or nil,
+
+	{ "nvim-neo-tree/neo-tree.nvim", dependencies = {
+		"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
+		config = function()
 			require("neo-tree").setup({
 				enable_git_status = false,
-				["event_handlers"] = {
+				event_handlers = {
 					{
-						["event"] = "file_opened",
-						["handler"] = function()
-							require("neo-tree.command").execute({ ["action"] = "close" })
+						event = "file_opened",
+						handler = function() require("neo-tree.command").execute({ action = "close" }) end,
+					}
+				},
+				window = {
+					position = "float",
+					mappings = {
+						Y = function(state)
+							local node = state.tree:get_node()
+							local filepath, filename = node:get_id(), node.name
+							local modify = vim.fn.fnamemodify
+							local results = {
+								filepath,
+								modify(filepath, ":."),
+								modify(filepath, ":~"),
+								filename,
+								modify(filename, ":r"),
+								modify(filename, ":e"),
+							}
+							local i = vim.fn.inputlist({
+								"Choose to copy to clipboard:",
+								"1. Absolute path: " .. results[1],
+								"2. Path relative to CWD: " .. results[2],
+								"3. Path relative to HOME: " .. results[3],
+								"4. Filename: " .. results[4],
+								"5. Filename without extension: " .. results[5],
+								"6. Extension of the filename: " .. results[6],
+							})
+							if i > 0 and results[i] then
+								vim.fn.setreg('"', results[i])
+								vim.notify("Copied: " .. results[i])
+							end
 						end,
 					},
 				},
-				["window"] = {
-					["position"] = "float",
-					["mappings"] = {
-						['Y'] = function(state)
-							local node = state.tree:get_node()
-							local filepath = node:get_id()
-							local filename = node.name
-							local modify = vim.fn.fnamemodify
-
-							local results = {
-								filepath,
-								modify(filepath, ':.'),
-								modify(filepath, ':~'),
-								filename,
-								modify(filename, ':r'),
-								modify(filename, ':e'),
-							}
-
-							local i = vim.fn.inputlist({
-								'Choose to copy to clipboard:',
-								'1. Absolute path: ' .. results[1],
-								'2. Path relative to CWD: ' .. results[2],
-								'3. Path relative to HOME: ' .. results[3],
-								'4. Filename: ' .. results[4],
-								'5. Filename without extension: ' .. results[5],
-								'6. Extension of the filename: ' .. results[6],
-							})
-
-							if i > 0 then
-								local result = results[i]
-								if not result then return print('Invalid choice: ' .. i) end
-								vim.fn.setreg('"', result)
-								vim.notify('Copied: ' .. result)
-							end
-						end
-					}
-				},
 			})
 			vim.keymap.set("n", "<leader>tt", function()
-				require("neo-tree.command").execute({
-					["reveal"] = true,
-					["position"] = "float",
-				})
+				require("neo-tree.command").execute({ reveal = true, position = "float" })
 			end)
-		end,
-	})
-
-	use("nvim-lua/plenary.nvim")
-
-	use({
-		"projekt0n/github-nvim-theme",
-		["config"] = function()
-			require('github-theme').setup()
-			vim.cmd.colorscheme("github_dark_tritanopia")
-		end,
-	})
-
-	use("archibate/lualine-time")
-
-	use({
-		"nvim-lualine/lualine.nvim",
-		["config"] = function()
-			local show_filename = function(str, ctx)
-				local path = vim.fn.expand("%:p")
-				if type(path) ~= "string" or #path < 1 then
-					path = "[new]"
-				end
-				local leave_space_for = 1
-				local hide_below = 5
-				local max_width = vim.fn.winwidth(ctx.tabnr) - leave_space_for
-				if max_width < hide_below then
-					return ""
-				end
-				if #path <= max_width then
-					return path
-				end
-				return "<"..path:sub(#path - max_width)
-			end
-			require("lualine").setup({
-				["options"] = {
-					["theme"] = "github_dark_tritanopia",
-				},
-				["sections"] = {
-					["lualine_a"] = {{
-						"filename",
-						["fmt"] = show_filename
-					}},
-					["lualine_b"] = {},
-					["lualine_c"] = {},
-					["lualine_x"] = {},
-					["lualine_y"] = {},
-					["lualine_z"] = {},
-				},
-				["inactive_sections"] = {
-					["lualine_a"] = {{
-						"filename",
-						["fmt"] = show_filename
-					}},
-					["lualine_b"] = {},
-					["lualine_c"] = {},
-					["lualine_x"] = {},
-					["lualine_y"] = {},
-					["lualine_z"] = {},
-				},
-				["inactive_winbar"] = {},
-				["winbar"] = {},
-				["tabline"] = {
-					["lualine_a"] = { "mode" },
-					["lualine_b"] = { "branch", "diff", "diagnostics" },
-					["lualine_c"] = { "encoding", "fileformat", "filetype" },
-					["lualine_x"] = { "cdate", "ctime" },
-					["lualine_y"] = { "progress", "location" },
-					["lualine_z"] = { "tabs" },
-				},
-			})
-		end,
-	})
-
-	if has_exec("git") then
-		use({
-			"tpope/vim-fugitive",
-			["config"] = function()
-				vim.keymap.set("n", "<leader>gs", ":Git<cr>", { ["silent"] = true })
-				vim.keymap.set("n", "<leader>gl", ":Git log --oneline<cr>", { ["silent"] = true })
-				vim.keymap.set("n", "<leader>gL", ":Git log --oneline %<cr>", { ["silent"] = true })
-				vim.keymap.set("n", "<leader>gb", ":Git blame<cr>", { ["silent"] = true })
-				vim.keymap.set("n", "<leader>gg", ":Git grep ")
-				vim.keymap.set("n", "<leader>gG", ":Git grep -in ")
-				vim.keymap.set("n", "<leader>gw", "yw:Git grep <c-f>p<cr>", { ["silent"] = true })
-				vim.keymap.set("n", "<leader>gW", "yw:Git grep -in <c-f>p<cr>", { ["silent"] = true })
-				vim.keymap.set("n", "<leader>ge", ":Gedit<cr>", { ["silent"] = true })
-			end,
-		})
-		if is_unix() then
-			use({
-				"airblade/vim-gitgutter",
-				["config"] = function()
-					vim.g.gitgutter_sign_added = "|"
-					vim.g.gitgutter_sign_modified = "|"
-				end
-			})
-		else
-			use({
-				"lewis6991/gitsigns.nvim",
-				["requires"] = {
-					"nvim-lua/plenary.nvim",
-				},
-				["config"] = function()
-					require("gitsigns").setup()
-					vim.keymap.set("n", "]c", function()
-						require("gitsigns").nav_hunk("next")
-					end)
-					vim.keymap.set("n", "[c", function()
-						require("gitsigns").nav_hunk("prev")
-					end)
-				end,
-			})
 		end
-	end
+	},
 
-	use({
-		"chrisgrieser/nvim-early-retirement",
-		["config"] = function ()
-			require("early-retirement").setup({})
-		end,
-	})
+	{ "projekt0n/github-nvim-theme", config = function()
+		require("github-theme").setup()
+		vim.cmd.colorscheme("github_dark_high_contrast")
+	end },
 
-	if has_env("SSH_CONNECTION") then
-		use({
-			"ojroques/nvim-osc52",
-			["config"] = function()
-				require('osc52').setup({
-					["max_length"] = 0,
-					["silent"] = false,
-					["trim"] = false,
-					["tmux_passthrough"] = true,
-				})
-				local function copy(lines, _)
-					require('osc52').copy(table.concat(lines, '\n'))
-				end
-				local function paste()
-					return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
-				end
-				vim.g.clipboard = {
-					["name"] =  "osc52",
-					["copy"] =  { ['+'] = copy, ['*'] = copy },
-					["paste"] = { ['+'] = paste, ['*'] = paste },
-				}
-			end
+	{ "archibate/lualine-time" },
+
+	{ "nvim-lualine/lualine.nvim", config = function()
+		local show_filename = function(_, ctx)
+			local path = vim.fn.expand("%:p")
+			if #path < 1 then path = "[new]" end
+			local max_width = vim.fn.winwidth(ctx.tabnr) - 1
+			if max_width < 5 then return "" end
+			return (#path <= max_width) and path or "<" .. path:sub(#path - max_width)
+		end
+		require("lualine").setup({
+			options = { theme = "github_dark_high_contrast" },
+			sections = {
+				lualine_a = { { "filename", fmt = show_filename } },
+				lualine_b = {}, lualine_c = {},
+				lualine_x = {}, lualine_y = {}, lualine_z = {},
+			},
+			inactive_sections = {
+				lualine_a = { { "filename", fmt = show_filename } },
+				lualine_b = {}, lualine_c = {},
+				lualine_x = {}, lualine_y = {}, lualine_z = {},
+			},
+			tabline = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "encoding", "fileformat", "filetype" },
+				lualine_x = { "cdate", "ctime" },
+				lualine_y = { "progress", "location" },
+				lualine_z = { "tabs" },
+			},
 		})
-	end
+	end },
 
-end)
+	has_exec("git") and {
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gs", ":Git<cr>", { silent = true })
+			vim.keymap.set("n", "<leader>gl", ":Git log --oneline<cr>", { silent = true })
+			vim.keymap.set("n", "<leader>gL", ":Git log --oneline %<cr>", { silent = true })
+			vim.keymap.set("n", "<leader>gb", ":Git blame<cr>", { silent = true })
+			vim.keymap.set("n", "<leader>gg", ":Git grep ")
+			vim.keymap.set("n", "<leader>gG", ":Git grep -in ")
+			vim.keymap.set("n", "<leader>gw", "yw:Git grep <c-f>p<cr>", { silent = true })
+			vim.keymap.set("n", "<leader>gW", "yw:Git grep -in <c-f>p<cr>", { silent = true })
+			vim.keymap.set("n", "<leader>ge", ":Gedit<cr>", { silent = true })
+		end,
+	} or nil,
+
+	(is_unix() and {
+		"airblade/vim-gitgutter", config = function()
+			vim.g.gitgutter_sign_added = "|"
+			vim.g.gitgutter_sign_modified = "|"
+		end
+	}) or {
+		"lewis6991/gitsigns.nvim", dependencies = { "nvim-lua/plenary.nvim" }, config = function()
+			require("gitsigns").setup()
+			vim.keymap.set("n", "]c", function() require("gitsigns").nav_hunk("next") end)
+			vim.keymap.set("n", "[c", function() require("gitsigns").nav_hunk("prev") end)
+		end
+	},
+
+	{ "chrisgrieser/nvim-early-retirement", config = function()
+		require("early-retirement").setup({})
+	end },
+
+	{
+		"0x00-ketsu/autosave.nvim",
+		event = { "InsertLeave", "TextChanged" },
+		config = function()
+			require("autosave").setup({
+				enabled = true,
+				execution_message = {
+					message = function() return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S")) end,
+					dim = 0.18,
+					cleaning_interval = 1250,
+				},
+				trigger_events = { "InsertLeave", "TextChanged" },
+				condition = function(buf)
+					return vim.bo[buf].modifiable and vim.bo[buf].buftype == ""
+				end,
+				debounce_delay = 1000, -- ms after typing stops
+				write_all_buffers = false, -- set to true to save all buffers
+			})
+		end,
+	},
+
+	has_env("SSH_CONNECTION") and {
+		"ojroques/nvim-osc52", config = function()
+			require("osc52").setup({ max_length = 0, silent = false, trim = false, tmux_passthrough = true })
+			local function copy(lines, _) require("osc52").copy(table.concat(lines, "\n")) end
+			local function paste() return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") } end
+			vim.g.clipboard = { name = "osc52", copy = { ["+"] = copy, ["*"] = copy }, paste = { ["+"] = paste, ["*"] = paste } }
+		end
+	} or nil,
+})
 
